9b7b791d0496d5ca34ed929bad93fb01a2174593
Cydeamon <loknhess@gmail.com>
2024-05-02 00:25:21 +0300

    Level loading. Window scaling

--- Sources/Main.gd
+++ Sources/Main.gd
@@ -1,8 +1,35 @@
 extends Node
 
+var level = null
+
 
 func _ready():
-	print("Hello, Godot!")
+	scale_window() # Remove this before exporting the game for the web
+	load_level("res://Levels/TestLevel.tscn")
+
 
 func _process(delta):
 	pass
+
+
+func scale_window():
+	var screen_size = DisplayServer.screen_get_size()
+	var window_size = DisplayServer.window_get_size()
+
+	while window_size.x * 2 < screen_size.x and window_size.y * 2 < screen_size.y:
+		window_size.x *= 2
+		window_size.y *= 2
+	
+	DisplayServer.window_set_size(window_size)
+	center_window()
+
+
+func load_level(path):
+	level = load(path).instantiate()
+	add_child(level)
+
+
+func center_window():
+	var screen_size = DisplayServer.screen_get_size()
+	var window_size = DisplayServer.window_get_size()
+	DisplayServer.window_set_position((screen_size / 2) - (window_size / 2))
