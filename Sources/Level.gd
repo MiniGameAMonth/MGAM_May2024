5b216fb25db3e2910b2d8e6aa03e5ac0874b6b7d
Cydeamon <loknhess@gmail.com>
2024-05-02 00:53:28 +0300

    Player scene. Spawning player on the level. Lighting

--- Sources/Level.gd
+++ Sources/Level.gd
@@ -3,9 +3,21 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	spawn_player()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func spawn_player():
+	var spawn_point = $PlayerSpawnPoint
+
+	if spawn_point == null:
+		push_error("Player spawn position not found on the level")
+
+	var player = load("res://GameObjects/Player.tscn").instantiate()
+	player.global_transform = spawn_point.global_transform
+	player.get_node("Camera").current = true
+	add_child(player)
