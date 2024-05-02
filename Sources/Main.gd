41ce1a3d0f81a6ca44875e946b32cfd6daa74ef3
Cydeamon <loknhess@gmail.com>
2024-05-02 21:07:16 +0300

    Player controls (WASD+Mouse, Mouse only)

--- Sources/Main.gd
+++ Sources/Main.gd
@@ -1,15 +1,31 @@
 extends Node
 
-var level = null
+enum ControlsMode { WASD_MOUSE, MOUSE_ONLY }
 
+#################################################################################
+################################### Variables ###################################
+
+@export var controls_mode = ControlsMode.WASD_MOUSE
+var level = null
+var local_delta = 0
+
+#################################################################################
+################################### Functions ###################################
 
 func _ready():
 	scale_window() # Remove this before exporting the game for the web
 	load_level("res://Levels/TestLevel.tscn")
 
+	# capture mouse
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 
 func _process(delta):
 	pass
+
+
+func _physics_process(delta):
+	local_delta = delta
 
 
 func scale_window():
@@ -30,6 +46,27 @@ func load_level(path):
 
 
 func center_window():
-	var screen_size = DisplayServer.screen_get_size()
+	var screen_size = DisplayServer.screen_get_size(1)
 	var window_size = DisplayServer.window_get_size()
 	DisplayServer.window_set_position((screen_size / 2) - (window_size / 2))
+
+
+
+func _input(event):
+	if event is InputEventMouseMotion:
+
+		if level:
+			if controls_mode == ControlsMode.MOUSE_ONLY:
+				if Input.is_action_pressed("Mouse Only - Strafe Mode"):
+					level.player.global_position += event.get_relative().x * local_delta * level.player.transform.basis.x
+
+				level.player.global_position += event.get_relative().y * local_delta * level.player.transform.basis.z
+
+
+func get_movement_input():
+	var result = Vector2(0, 0)
+
+	if controls_mode == ControlsMode.WASD_MOUSE:
+		result = Input.get_vector("WASD+Mouse - Move Left", "WASD+Mouse - Move Right", "WASD+Mouse - Move Forward", "WASD+Mouse - Move Backward")
+
+	return result
--- Sources/Player.gd
+++ Sources/Player.gd
@@ -1,11 +1,37 @@
-extends Node3D
+extends CharacterBody3D
+
+const SPEED = 10.0
+
+@export var mouse_sensitivity = 0.1
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var root_node
+var local_delta = 0
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	root_node = get_tree().root.get_child(0)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	local_delta = delta
+	# Movement
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	var input_dir = root_node.get_movement_input()
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		if !Input.is_action_pressed("Mouse Only - Strafe Mode"):
+			rotation.y -= event.get_relative().x * mouse_sensitivity * local_delta
