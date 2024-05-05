94ec0ecef865061671c4590c3fb39020dec4d2ad
KoalaPanda54 <31424736+KoalaPanda54@users.noreply.github.com>
2024-05-06 01:09:39 +0200

    Cleaning and targeting fix.
    Fixed projectile targeting.
    Changed collision masks on mushrooms and enemies.
    Cleaned up code.
    Cleaned up project structure.

---
+++ Sources/Weapons/Projectile.gd
@@ -0,0 +1,54 @@
+class_name Projectile
+extends Node3D
+
+@export var speed: float = 0
+@export var life_time: float = 0
+@export var target: Node3D = null
+
+var damage: float = 0
+var direction: Vector3 = Vector3.ZERO
+var canvas;
+
+func _ready():
+	var timer : Timer = Timer.new()
+	add_child(timer)
+	timer.connect("timeout", _on_timeout)
+	timer.start(life_time)
+
+	check_for_target()
+
+func set_direction(dir: Vector3) -> void:
+	direction = dir
+	check_for_target()
+
+func set_speed(spd: float) -> void:
+	speed = spd
+
+func set_target(tgt: Node3D) -> void:
+	target = tgt
+	direction = (target.global_transform.origin - global_transform.origin).normalized()
+
+func _physics_process(delta):
+	global_transform.origin += direction * speed * delta
+
+
+func _on_timeout():
+	queue_free()
+
+func _on_area_2d_body_entered(body:Node2D):    
+	if body is EntityHitbox:
+		body.character.take_damage(damage)
+		queue_free()
+
+func check_for_target():
+	var area : Area2D = get_node("ProjectedNode/Area2D")
+	var space_state = area.get_world_2d().direct_space_state
+	var starting_point = Vector2(global_position.x, global_position.z) * Projected2DNode.PIXEL_PER_METER    
+	var bullet_direction = Vector2(direction.x, direction.z) * 25 * Projected2DNode.PIXEL_PER_METER
+
+	var query = PhysicsRayQueryParameters2D.create(starting_point, starting_point + bullet_direction)
+	var result = space_state.intersect_ray(query)
+	
+	if result:
+		if result.collider is EntityHitbox:
+			set_target(result.collider.character)
--- Sources/Weapons/Weapon.gd
+++ Sources/Weapons/Weapon.gd
@@ -4,26 +4,22 @@ extends Node3D
 @export var attackDistance : float = 0 
 @export var weaponDamage : float = 0
 @export var cooldownTime : float = 0 
-@export var bullet : PackedScene
-@export var player : Node3D
 
 @onready var cooldown : Timer = $Cooldown
 
 var isTimerOver : bool = true
+var bullet;
 
 func _ready():
+	bullet = preload("res://GameObjects/Bullet.tscn")
 	isTimerOver = true
 	if bullet == null:
-		print("Bullet is not set")
-		return
+		push_error("Projectile is not set.")
 
 func is_target_in_range(_target : Node3D):
 	var distance = global_position.distance_to(_target.global_position)
 
-	if distance <= attackDistance:
-		return true
-	else:
-		return false
+	return distance <= attackDistance
 
 func try_deal_damage(_target : Node3D):
 	if isTimerOver:
@@ -33,13 +29,11 @@ func try_deal_damage(_target : Node3D):
 
 func shoot(): 	
 	if isTimerOver:
-		var playerDirection : Vector3 = -player.global_transform.basis.z
-
-		var bulletInstance : Projectile = load("GameObjects/Bullet.tscn").instantiate()
+		var bulletInstance : Projectile = bullet.instantiate()
 		get_tree().root.add_child(bulletInstance)
 
 		bulletInstance.global_position = global_position
-		bulletInstance.set_direction(playerDirection)
+		bulletInstance.set_direction(global_transform.basis.z)
 		bulletInstance.damage = weaponDamage     
 	
 func _on_cooldown_timeout():
